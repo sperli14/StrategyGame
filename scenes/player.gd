8734991962f58de5f8ef0d6d2a788d607b18de8a
sperli14 <sperli14@msu.edu>
2026-03-27 15:38:29 -0400

    All day map ground added
    -screen wrapping also adjusted

--- scenes/player.gd
+++ scenes/player.gd
@@ -6,14 +6,16 @@ const JUMP_VELOCITY = -400.0
 const FLAP_VELOCITY = -40.0
 const MAX_FLIGHT_SPEED = -600.0
 const MAX_FALL_SPEED = 700.0
+const CEILING_BOUNCE = 0.8 #how elastic are collisions with ceiling - 1 = elastic
 
-@onready var screen_size = [2000, 1200]#DisplayServer.screen_get_size()#get_viewport_rect().size
+@onready var screen_size = [2050, 1200]#DisplayServer.screen_get_size()#get_viewport_rect().size
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	if is_on_ceiling() and velocity.y < -10:
+		velocity.y = velocity.y * -1 * CEILING_BOUNCE
 	# Handle jump.
 	#if Input.is_action_just_pressed("ui_accept"):
 	if Input.is_action_pressed("ui_accept"):
